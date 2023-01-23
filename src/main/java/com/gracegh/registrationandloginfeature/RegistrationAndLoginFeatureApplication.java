4820307082ead863bbac6da33b786f3b0dced326
GraceG000 <graceghansah2000@gmail.com>
2023-01-23 12:43:41 +0000

    Initial commit

---
+++ src/main/java/com/gracegh/registrationandloginfeature/RegistrationAndLoginFeatureApplication.java
@@ -0,0 +1,13 @@
+package com.gracegh.registrationandloginfeature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationAndLoginFeatureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrationAndLoginFeatureApplication.class, args);
+	}
+
+}
