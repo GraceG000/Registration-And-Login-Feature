55bc49f8439e6ee9b1ecb2c49670f44539c3c708
GraceG000 <graceghansah2000@gmail.com>
2023-01-25 15:47:26 +0000

    User Entity

---
+++ src/main/java/com/gracegh/registrationandloginfeature/Repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.gracegh.registrationandloginfeature.Repository;
+
+import com.gracegh.registrationandloginfeature.Entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+
+}
---
+++ src/test/java/com/gracegh/registrationandloginfeature/Repository/UserRepositoryTest.java
@@ -0,0 +1,41 @@
+package com.gracegh.registrationandloginfeature.Repository;
+
+import com.gracegh.registrationandloginfeature.Entity.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@DataJpaTest//to make sure that the changes are made to the databases itself...
+    @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private TestEntityManager testEntityManager;
+
+
+    @Test
+    public void testCreateUser(){
+        User user = new User();
+
+        user.setEmail("adrienag99@gmail.com");
+        user.setPassword("iHeartmari!");
+        user.setFirstName("Adrien");
+        user.setLastName("Agreste");
+
+        User savedUser = userRepository.save(user);
+
+        User existUser = testEntityManager.find(User.class, savedUser.getId());
+
+        assertThat(existUser.getEmail()).isEqualTo(user.getEmail());
+    }
+}
